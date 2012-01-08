a58272ba47b84020b88a490b92303109ca5b41b1
Matt Campbell <matt@mattcampbell.net>
2012-01-07 21:05:52 -0500

    Setting up mouse events

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function () {
-	var context;
+	var context, app;
 
 	function createCanvas(width, height, node) {
 		var canvas = document.createElement('canvas');
@@ -10,8 +10,56 @@
 			node.appendChild(canvas);
 		}
 
+		['mouseMove', 'mouseDown', 'mouseUp'].forEach(function (event) {
+			canvas.addEventListener(event.toLowerCase(), function (e) {
+				var x, y;
+				x = e.offsetX;
+				y = e.offsetY;
+
+				if (typeof x === 'undefined' || typeof y === 'undefined') {
+					x = e.pageX - canvas.offsetLeft;
+					y = e.pageY - canvas.offsetTop;
+				}
+
+				app[event].apply(app, [x, y, e]);
+				update();
+			}, false);
+		});
+
 		return canvas.getContext('2d');
 	}
 
+	function update() {}
+
 	context = createCanvas(800, 600, document.body);
+
+	app = (function () {
+		var mouse;
+
+		mouse = {
+			x: -1,
+			y: -1,
+			isDown: false
+		};
+
+		return {
+			mouseMove: function (x, y) {
+				mouse.x = x;
+				mouse.y = y;
+				console.log(mouse.x, mouse.y, mouse.isDown);
+			},
+
+			mouseDown: function (x, y) {
+				mouse.x = x;
+				mouse.y = y;
+				mouse.isDown = true;
+			},
+
+			mouseUp: function (x, y) {
+				mouse.x = x;
+				mouse.y = y;
+				mouse.isDown = false;
+			}
+		};
+	}());
 }());
