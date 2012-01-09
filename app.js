b3254fd87bc949f74126d5237a95d4daee3521e7
Matt Campbell <matt@mattcampbell.net>
2012-01-08 23:25:55 -0500

    Converting path into reusable prototype

--- app.js
+++ app.js
@@ -35,6 +35,72 @@
 	height = 600;
 	context = createCanvas(width, height, document.body);
 
+	function Path(input) {
+		if (typeof input === 'undefined') {
+			this.path = [];
+		} else {
+			this.deserialize(input);
+		}
+	}
+
+	Path.prototype.addPoint = function (x, y) {
+		var tail;
+
+		if (this.path.length) {
+			tail = this.path.slice(-1);
+			if (x === tail.x && y === tail.y) {
+				return false;
+			}
+		}
+
+		return this.path.push({ x: +x, y: +y });
+	};
+
+	Path.prototype.serialize = function () {
+		return this.path.map(function (point) {
+			return [point.x, point.y].join(',');
+		})
+		.join(',')
+		.split(',')
+		.map(function (n) {
+			var convert = (+n).toString(16);
+			while (convert.length < 3) {
+				convert = '0' + convert;
+			}
+
+			return convert;
+		})
+		.join('');
+	};
+
+	Path.prototype.deserialize = function (input) {
+		var matches = input.match(/[0-9a-f]{6}/g);
+
+		if (!matches) {
+			return false;
+		}
+
+		this.path = matches.map(function (n) {
+			return ({
+				x: parseInt(n.slice(0, 3), 16),
+				y: parseInt(n.slice(3), 16)
+			});
+		});
+	};
+
+	Path.prototype.render = function (ctx) {
+		if (this.path.length) {
+			ctx.beginPath();
+			ctx.moveTo(this.path[0].x, this.path[0].y);
+
+			for (j = 1; j < this.path.length; j += 1) {
+				ctx.lineTo(this.path[j].x, this.path[j].y);
+			}
+
+			ctx.stroke();
+		}
+	};
+
 	app = (function () {
 		var mouse, path;
 
@@ -44,56 +110,7 @@
 			isDown: false
 		};
 
-		path = [];
-
-		path.addPoint = function (x, y) {
-			var tail;
-
-			if (path.length) {
-				tail = path.slice(-1);
-				if (x === tail.x && y === y) {
-					return false;
-				}
-			}
-
-			return path.push({ x: +x, y: +y });
-		};
-
-		path.serialize = function () {
-			return path.map(function (point) {
-				return [point.x, point.y].join(',');
-			})
-			.join(',')
-			.split(',')
-			.map(function (n) {
-				var convert = (+n).toString(16);
-				while (convert.length < 3) {
-					convert = '0' + convert;
-				}
-
-				return convert;
-			})
-			.join('');
-		};
-
-		path.deserialize = function (input) {
-			var matches = input.match(/[0-9a-f]{6}/g);
-
-			if (!matches) {
-				return false;
-			}
-
-			while (path.length) {
-				path.pop();
-			}
-
-			matches.forEach(function (n) {
-				path.push({
-					x: parseInt(n.slice(0, 3), 16),
-					y: parseInt(n.slice(3), 16)
-				});
-			});
-		};
+		path = new Path();
 
 		function render(ctx) {
 			var j;
@@ -101,17 +118,7 @@
 			ctx.clearRect(0, 0, width, height);
 
 			ctx.strokeStyle = '#000';
-
-			if (path.length) {
-				ctx.beginPath();
-				ctx.moveTo(path[0].x, path[0].y);
-
-				for (j = 1; j < path.length; j += 1) {
-					ctx.lineTo(path[j].x, path[j].y);
-				}
-
-				ctx.stroke();
-			}
+			path.render(ctx);
 		}
 
 		return ({
